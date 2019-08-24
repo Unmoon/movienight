e993cd386c1daf721cdaf15e462a0951ebc07f41
Unmoon <joona@unmoon.com>
2019-08-24 22:13:38 +0300

    Update setup.py description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/Unmoon/movienight",
     author="Unmoon",
     author_email="joona@unmoon.com",
-    description="Simple VLC-based client that syncs video playback with remote server and other clients.",
+    description="VLC-based media player that syncs with other players using Movie Night Server.",
     packages=find_packages(where=""),
     package_dir={"movienight": "movienight"},
     entry_points={"console_scripts": ["movienight=movienight.main:main"]},
